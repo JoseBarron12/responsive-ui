bd94e46bb558e60fb151d02623a2588dc3251a32
JoseBarron12 <143546100+JoseBarron12@users.noreply.github.com>
2025-12-29 22:02:12 -0600

    feat: add array of project obj.
    Each project obj contain information for DOM creation
    of each within project section in main section
    through js.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,55 @@
 import "./styles.css"
+import battleshipImg from "./images/battleship.jpg"
+import weatherImg from "./images/weather.jpg"
+import todoImg from "./images/todo.jpg"
+import resturantImg from "./images/resturant.jpg"
+import ticImg from "./images/tictactoe.jpg"
+import adminImg from "./images/admin.jpg"
+
+const projects = [
+    {
+        name: 'BattleShip Game',
+        githubLink: 'https://github.com/JoseBarron12/battleship-game',
+        liveLink: 'https://josebarron12.github.io/battleship-game/',
+        desc: 'A browser-based Battleship game where you place your ships, choose attack coordinates, and battle against a computer opponent.',
+        img: battleshipImg
+    },
+    {
+        name: 'Weather App',
+        githubLink: 'https://github.com/JoseBarron12/weather-app',
+        liveLink: 'https://josebarron12.github.io/weather-app/',
+        desc: 'A simple and elegant Weather App built using JavaScript, HTML, and CSS, inspired by the iOS Weather App design.',
+        img: weatherImg
+    },
+    {
+        name: 'To-Do-List',
+        githubLink: 'https://github.com/JoseBarron12/to-do-list',
+        liveLink: 'https://josebarron12.github.io/to-do-list/',
+        desc: 'A simple yet powerful To-Do List web app that lets you create and manage tasks and projects/lists right in your browser.',
+        img: todoImg
+    },
+    {
+        name: 'Resturant Page',
+        githubLink: 'https://github.com/JoseBarron12/resturant-page',
+        liveLink: 'https://josebarron12.github.io/to-do-list/',
+        desc: 'A simple website for a resturant including a welcome, menu, and about page',
+        img: resturantImg
+    },
+    {
+        name: 'Tic-Tac-Toe',
+        githubLink: 'https://github.com/JoseBarron12/tic-tac-toe',
+        liveLink: 'https://josebarron12.github.io/tic-tac-toe/',
+        desc: 'A browser-based tic-tac-toe game',
+        img: ticImg
+    },
+    {
+        name: 'Admin Page',
+        githubLink: 'https://github.com/JoseBarron12/admin-page',
+        liveLink: 'https://josebarron12.github.io/admin-page/',
+        desc: 'A simple static admin dashboard',
+        img: adminImg
+    },
+
+]
 
 
