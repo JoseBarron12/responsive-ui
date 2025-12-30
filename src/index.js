2738d1341ac1c0820f84202fc4ac40696a2de9c0
JoseBarron12 <143546100+JoseBarron12@users.noreply.github.com>
2025-12-30 01:43:13 -0600

    feat: add project html elements.
    Project section no longer empty but display
    project info.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import todoImg from "./images/todo.jpg"
 import resturantImg from "./images/resturant.jpg"
 import ticImg from "./images/tictactoe.jpg"
 import adminImg from "./images/admin.jpg"
+import linkSvg from "./images/link.svg"
 
 const projects = [
     {
@@ -49,7 +50,71 @@ const projects = [
         desc: 'A simple static admin dashboard',
         img: adminImg
     },
-
 ]
+
+const projectSection = document.querySelector(".project-section");
+
+for(const proj of projects)
+{
+    const project = document.createElement("div");
+    project.classList.add("project");
+    projectSection.appendChild(project);
+
+    const projImg = document.createElement("img");
+    projImg.classList.add("project-image");
+    //projImg.src = proj.img;
+    project.appendChild(projImg);
+
+    const projDesc = document.createElement("div");
+    projDesc.classList.add("project-info");
+    project.appendChild(projDesc);
+
+    const projHeader = document.createElement("div");
+    projHeader.classList.add("project-header");
+    projDesc.appendChild(projHeader);
+
+    const projTitle = document.createElement("h4");
+    projTitle.classList.add("project-title");
+    projTitle.textContent = proj.name;
+    projHeader.appendChild(projTitle);
+
+    const projIcons = document.createElement("div");
+    projIcons.classList.add("project-icons")
+    projHeader.appendChild(projIcons);
+
+    const githubLink = document.createElement("a");
+    githubLink.setAttribute("href", proj.githubLink);
+    projIcons.appendChild(githubLink);
+    
+    const githubText = document.createElement("p");
+    githubText.textContent = "GitHub Project Repository (opens in new tab)";
+    githubText.classList.add("link-text");
+    githubLink.appendChild(githubText);
+
+    const githubIcon = document.createElement("img");
+    githubIcon.classList.add("project-icon");
+    githubIcon.src = "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg";
+    githubLink.appendChild(githubIcon);
+
+    const liveLink = document.createElement("a");
+    liveLink.setAttribute("href", proj.liveLink);
+    projIcons.appendChild(liveLink);
+    
+    const liveText = document.createElement("p");
+    liveText.textContent = "Live Project(opens in new tab)";
+    liveText.classList.add("link-text");
+    liveLink.appendChild(liveText);
+
+    const liveIcon = document.createElement("img");
+    liveIcon.classList.add("project-icon");
+    liveIcon.src = linkSvg;
+    liveLink.appendChild(liveIcon);
+
+    const projInfo = document.createElement("p");
+    projInfo.textContent = proj.desc;
+    projDesc.appendChild(projInfo);
+    
+
+}
 
 
